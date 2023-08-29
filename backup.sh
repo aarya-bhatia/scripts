c01a0d9a3156428dba83f648ae63ee7b114ede58
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-08-29 16:37:25 -0500

    update

--- backup.sh
+++ backup.sh
@@ -7,8 +7,28 @@ if ! [ -f $COPYIGNORE ]; then
   exit 1
 fi
 
-backup_src="/home/aarya"
-backup_dest="/mnt/aarya/backup"
+TARGET_DIR=$1
+
+if [ -z $TARGET_DIR ]; then
+  echo "Usage: $0 target_dir"
+  exit 1
+fi
+
+if [ ! -d $TARGET_DIR ]; then
+  echo "Target directory does not exist."
+  exit 1
+fi
+
+backup_src=$HOME
+backup_dest=$(realpath "$TARGET_DIR")
+
+if [ ! -e "$backup_dest" ]; then
+    echo "The path does not exist."
+	exit 1
+fi
+
+echo "Source directory: $backup_src"
+echo "Destination directory: $backup_dest"
 
 sudo rsync -aP --update --exclude-from=$COPYIGNORE --no-o --no-g \
   $backup_src $backup_dest
--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-status=$(curl -X HEAD -I https://aaryab.in  | grep HTTP | cut -d ' ' -f2)
+status=$(curl --max-time 5 -X HEAD -I https://aaryab.in/ping  | grep HTTP | cut -d ' ' -f2)
 
 if [ $status != "200" ]
 then
   notify-send -t 2000 "ALERT: aaryab.in is not available"
 fi
+
+echo "test" >> /home/aarya/crontab.log
