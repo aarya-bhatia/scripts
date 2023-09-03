454d5e7dc09a51754efc9d21b032b5317617ba89
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-09-02 19:48:14 -0500

    lf trash script

--- lf/trash.py
+++ lf/trash.py
@@ -1,19 +1,32 @@
 import sys
+import os
 
-for filename in sys.stdin:
-    print(filename)
+# Get the value of the $fx environment variable
+fx = os.environ.get('fx', '')
 
-    if "GoogleDrive/" in filename:
-        start = filename.index("GoogleDrive")
-        start += len("GoogleDrive/")
-        path = filename[start:]
-        if len(path) > 0:
-            print(path)
+# Split the filenames by newline characters
+filenames = fx.split()
 
-    if "OneDrive/" in filename:
-        start = filename.index("OneDrive")
-        start += len("OneDrive/")
-        path = filename[start:]
-        if len(path) > 0:
-            print(path)
+# Iterate through the filenames and print them line by line
+for filename in filenames:
+    ans = input(f"delete {filename} [y/n]?")
+    if ans == 'y':
+        os.remove(filename)
 
+# for filename in sys.stdin:
+#     print(filename)
+#
+#     if "GoogleDrive/" in filename:
+#         start = filename.index("GoogleDrive")
+#         start += len("GoogleDrive/")
+#         path = filename[start:]
+#         if len(path) > 0:
+#             print(path)
+#
+#     if "OneDrive/" in filename:
+#         start = filename.index("OneDrive")
+#         start += len("OneDrive/")
+#         path = filename[start:]
+#         if len(path) > 0:
+#             print(path)
+#
