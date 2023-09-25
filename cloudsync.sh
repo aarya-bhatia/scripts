88c437060b770139e583e825e5a71865cb7b2ba6
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-09-25 08:58:38 -0500

    Synced on 09/25/2023 08:58:38 AM

--- cloudsync.sh
+++ cloudsync.sh
@@ -9,6 +9,6 @@ fi
 
 rclone copy --exclude-from=$COPYIGNORE -v --update ~/GoogleDrive gdrive:
 rclone copy --exclude-from=$COPYIGNORE -v --update gdrive: ~/GoogleDrive
-rclone copy --exclude-from=$COPYIGNORE -v --update ~/OneDrive onedrive:
-rclone copy --exclude-from=$COPYIGNORE -v --update onedrive: ~/OneDrive
+# rclone copy --exclude-from=$COPYIGNORE -v --update ~/OneDrive onedrive:
+# rclone copy --exclude-from=$COPYIGNORE -v --update onedrive: ~/OneDrive
 
