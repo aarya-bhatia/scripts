0b1d6a405b50c932bc961a4c2248d78ec0ba771d
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-10-01 16:36:59 -0500

    Synced on 10/01/2023 04:36:59 PM

--- keymaps.sh
+++ keymaps.sh
@@ -3,6 +3,8 @@
 # Map CapsLock to Control when pressed with another key
 setxkbmap -option 'caps:ctrl_modifier'
 
+killall xcape 2>/dev/null
+
 # Run xcape to make CapsLock act as Escape when pressed and released alone
 xcape -e 'Caps_Lock=Escape'
 
