31557a92ec50403757316c5f0ef1744c3f9adf51
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-10-03 22:57:35 -0500

    Synced on 10/03/2023 10:57:35 PM

--- brightness.sh
+++ brightness.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
 if command -v brightnessctl > /dev/null 2>&1; then
-	max_brightness=$(brightnessctl m)
-	cur_brightness=$(brightnessctl get)
-	percentage=$(python -c "print(round(100*$cur_brightness/$max_brightness))")
-	printf "%s%%" $percentage
+	brightnessctl | grep -oP "\([\d]+%\)" | sed "s/(\|)//g"
+	# max_brightness=$(brightnessctl m)
+	# cur_brightness=$(brightnessctl get)
+	# percentage=$(python -c "print(round(100*$cur_brightness/$max_brightness))")
+	# printf "%s%%" $percentage
 else
 	printf '0'
 fi
