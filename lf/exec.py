a1c26f778286f644a83943e246abd61b65e13758
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-10-06 16:44:29 -0500

    Synced on 10/06/2023 04:44:29 PM

--- lf/exec.py
+++ lf/exec.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import sys
 import subprocess
 
 # Get selected file
@@ -7,9 +8,15 @@ filename = os.environ.get('f', '')
 cwd = os.environ.get('OLDPWD', os.curdir)
 print(cwd, filename)
 
+args = ""
+
 if os.path.isfile(filename) and os.access(filename, os.X_OK):
-    args = input("Args: ").strip().split()
-    status = subprocess.call([filename, *args], cwd=cwd)
+    if len(sys.argv) > 1 and sys.argv[1] == "args":
+        args = input("Args: ").strip().split()
+        status = subprocess.call([filename, *args], cwd=cwd)
+    else:
+        status = subprocess.call([filename], cwd=cwd)
+
     if status != 0:
         print("Program failed with status code: ", status)
 else:
