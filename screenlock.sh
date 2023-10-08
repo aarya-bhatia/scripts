1714e2ea114a5e2999b4d82ce5b6fabdb7a9edd2
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-10-07 20:55:34 -0500

    Synced on 10/07/2023 08:55:34 PM

--- screenlock.sh
+++ screenlock.sh
@@ -2,8 +2,11 @@
 # screensaver='/usr/share/backgrounds/archlinux/wild.png'
 screensaver='~/wallpapers/0001.jpg'
 message='The screen will lock soon.'
-xautolock -time 1 \
-  -locker "~/.local/bin/betterlockscreen --lock" \
-  -notify 10 --notifier "notify-send '$message'" \
-  -detectsleep -killtime 10 -killer "systemctl suspend" &
+locker='~/.local/bin/betterlockscreen --lock'
+
+killall -q xautolock
+
+xautolock -time 10 -locker "$locker" \
+		-notify 15 -notifier "notify-send $message" \
+		-detectsleep -killtime 10 -killer "systemctl suspend" &
 
