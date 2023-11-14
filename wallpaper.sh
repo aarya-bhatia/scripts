fb65b7a6831ad6da738edcae5f40a00fbac941a9
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-11-13 18:12:37 -0600

    Synced on 11/13/2023 06:12:37 PM

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WALLPAPER_DIR='/home/aarya/wallpapers'
 wallpaper=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
-echo $wallpaper
+echo $wallpaper >> ~/wallpaper.log
 killall -q feh
 feh --no-fehbg --bg-scale "$wallpaper"
