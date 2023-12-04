b9aaf2d123da6d51b56079ffd927638a2787b4aa
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-12-03 21:45:05 -0600

    Synced on 12/03/2023 09:45:05 PM

--- sync.sh
+++ sync.sh
@@ -19,12 +19,11 @@ copyignore="/home/aarya/dotfiles/copyignore"
 opts="-avu --exclude-from=$copyignore"
 directories=(
 "$HOME/.config/i3"
+"$HOME/.config/i3status"
 "$HOME/.config/polybar"
 "$HOME/.config/terminator"
-"$HOME/.config/rofi"
 "$HOME/.config/nvim"
 "$HOME/.config/zathura"
-"$HOME/.config/qutebrowser"
 "$HOME/.config/lf"
 )
 
@@ -33,7 +32,6 @@ for directory in "${directories[@]}"; do
 done
 
 rsync $opts $HOME/.xinitrc $HOME/dotfiles/xinitrc
-rsync $opts $HOME/.Xmodmap $HOME/dotfiles/Xmodmap
 rsync $opts $HOME/.gitconfig $HOME/dotfiles/gitconfig
 rsync $opts $HOME/.agignore $HOME/dotfiles/agignore
 rsync $opts $HOME/.clang-format $HOME/dotfiles/clang-format
@@ -46,10 +44,12 @@ crontab -l > $HOME/dotfiles/crontab
 cd $HOME/dotfiles
 git add .
 git commit -m "Synced on $(date +'%x %X')"
+git push
 
 cd $HOME/scripts
 git add .
 git commit -m "Synced on $(date +'%x %X')"
+git push
 
 yes=0
 
