171f6c56b87bc912ce9f7918331da405a9e43b0e
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-12-16 16:34:41 -0600

    Synced on 12/16/2023 04:34:41 PM

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 WALLPAPER_DIR='/home/aarya/wallpapers'
-wallpaper=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
-echo $wallpaper >> ~/wallpaper.log
-killall -q feh
-feh --no-fehbg --bg-scale "$wallpaper"
+wallpaper=$(find "$WALLPAPER_DIR" -type f | grep -E "\.png$|\.jpg$|\.jpeg$" | shuf -n 1)
+
+# if which nitrogen; then
+# 	killall -q nitrogen
+# 	nitrogen "$wallpaper" && echo $wallpaper >> ~/wallpaper.log
+if which feh; then
+	killall -q feh
+	feh --no-fehbg --bg-scale "$wallpaper" && echo $wallpaper >> ~/wallpaper.log
+else
+	notify-send "aarya" "failed to set wallpaper"
+fi
+
