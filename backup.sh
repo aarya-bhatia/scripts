10e5e393ab051cfe301b5953488a957782102ae4
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-12-18 21:04:56 -0600

    Synced on 12/18/2023 09:04:56 PM

--- backup.sh
+++ backup.sh
@@ -1,67 +1,29 @@
 #!/bin/bash
 
-echo $COPYIGNORE;
-
 if ! [ -f $COPYIGNORE ]; then
-  echo "ERROR"
-  exit 1
-fi
-
-TARGET_DIR=$1
-
-if [ -z $TARGET_DIR ]; then
-  echo "Usage: $0 target_dir"
-  exit 1
-fi
-
-if [ ! -d $TARGET_DIR ]; then
-  echo "Target directory does not exist."
-  exit 1
-fi
-
-backup_src=$HOME
-backup_dest=$(realpath "$TARGET_DIR")
-
-if [ ! -e "$backup_dest" ]; then
-    echo "The path does not exist."
+	echo "COPYIGNORE is not set"
 	exit 1
 fi
 
-echo "Source directory: $backup_src"
-echo "Destination directory: $backup_dest"
-
-# copydirs=(
-# 	"$HOME/Backup/"
-# 	"$HOME/repos/"
-# 	"$HOME/GoogleDrive/"
-# 	"$HOME/Documents/"
-# 	"$HOME/Desktop/"
-# 	"$HOME/Library/"
-# 	"$HOME/wallpapers/"
-# 	"$HOME/Development/"
-# 	"$HOME/Downloads/"
-# 	"$HOME/go/"
-# )
-#
-# for src in "${copydirs[@]}"; do
-# 	echo "Source directory: $src"
-# 	sudo rsync -aP --update --exclude-from=$COPYIGNORE --no-o --no-g \
-# 	  $src $backup_dest
-# done
-
-sudo rsync --archive --progress --human-readable --delete --update \
-	--exclude-from=$COPYIGNORE --no-owner --no-group \
-	  $backup_src $backup_dest
-
-public_key_path="$backup_dest/public.pgp"
-private_key_path="$backup_dest/private.pgp"
-
-if [ ! -e $public_key_path ]; then
-	gpg --armor --export aarya.bhatia1678@gmail.com | sudo tee $public_key_path >/dev/null
+if ! which rsync; then
+	echo "rsync is not installed"
+	exit 1
 fi
 
-if [ ! -e $private_key_path ]; then
-	gpg --armor --export-secret-key aarya.bhatia1678@gmail.com | sudo tee $private_key_path >/dev/null
+echo "backing up /home/$USER/ to /mnt/$USER"
+
+sudo rsync --archive --progress --human-readable --delete --update \
+	--exclude-from=$COPYIGNORE --no-owner --no-group /home/$USER/ /mnt/$USER/
+
+pubkey="$backup_dest/public.pgp"
+privkey="$backup_dest/private.pgp"
+
+if ! [ -e $pubkey ]; then
+	gpg --armor --export aarya.bhatia1678@gmail.com | sudo tee $pubkey >/dev/null
+fi
+
+if ! [ -e $privkey ]; then
+	gpg --armor --export-secret-key aarya.bhatia1678@gmail.com | sudo tee $privkey >/dev/null
 fi
 
 echo "Backup created in $backup_dest"
