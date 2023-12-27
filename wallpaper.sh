fc3511d0e5ff1dddc071224efbb3cf48f77f290e
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-12-27 10:20:46 +0530

    Synced on 27/12/23 10:20:46 AM IST

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 WALLPAPER_DIR='/home/aarya/wallpapers'
-wallpaper=$(find "$WALLPAPER_DIR" -type f | grep -E "\.png$|\.jpg$|\.jpeg$" | shuf -n 1)
+wallpaper=$(find "$WALLPAPER_DIR" -type f | grep -E "\.png|\.jpg|\.jpeg" | shuf -n 1 | cut -d" " -f1)
 
-# if which nitrogen; then
-# 	killall -q nitrogen
-# 	nitrogen "$wallpaper" && echo $wallpaper >> ~/wallpaper.log
 if which feh; then
 	killall -q feh
 	feh --no-fehbg --bg-scale "$wallpaper" && echo $wallpaper >> ~/wallpaper.log
