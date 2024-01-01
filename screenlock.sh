f175472fc4eea158d4dd99d0749603296f144dd1
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-01-01 08:19:45 +0530

    Synced on 01/01/24 08:19:45 AM IST

--- screenlock.sh
+++ screenlock.sh
@@ -2,14 +2,14 @@
 screensaver='~/wallpapers/0001.jpg'
 message='The screen will lock soon.'
 # locker='betterlockscreen --lock'
-locker='i3lock'
-
-killall -q xautolock
+locker='i3lock -c 009999'
 
 locktime=25
 killtime=15
 
+killall -q xautolock
+
 xautolock -time $locktime -locker "$locker" \
 		-notify 15 -notifier "notify-send $message" \
-		-detectsleep -killtime $killtime -killer "systemctl suspend" &
+		-detectsleep -killtime $killtime -killer "systemctl suspend"
 
