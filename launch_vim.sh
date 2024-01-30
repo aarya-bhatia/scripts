2adcdf68b2c49056a53d94afa32acecc018a6ff3
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-01-29 20:31:33 -0600

    script to launch and copy text from vim buffer

---
+++ launch_vim.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+source /home/aarya/.bash_profile
+tmpfile=$(mktemp)
+file $tmpfile
+
+alacritty --class=floating-vim -e /usr/local/nvim/bin/nvim $tmpfile
+size=$(du -b $tmpfile | cut -f1)
+if [ $size -gt 0 ]; then
+	cat $tmpfile | xsel -b
+fi
+rm $tmpfile
+
