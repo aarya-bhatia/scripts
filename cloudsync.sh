5435b2128ef4e98b194312d97cc9accd4c81c1e5
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-01-31 22:33:40 -0600

    Synced on 01/31/2024 10:33:40 PM

--- cloudsync.sh
+++ cloudsync.sh
@@ -10,7 +10,17 @@ fi
 read -p "override remote files with local files [y/n]?" ans
 if [ $ans = 'y' ]; then
 	rclone sync --exclude-from=$COPYIGNORE -v ~/GoogleDrive gdrive:
+	exit 0
 else
+	read -p "override local files with remote files [y/n]?" ans
+	if [ $ans = 'y' ]; then
+		rclone sync --exclude-from=$COPYIGNORE -v gdrive: ~/GoogleDrive
+		exit 0
+	fi
+
+	echo "running bidirectional copy"
+
+	rclone sync --exclude-from=$COPYIGNORE -v ~/GoogleDrive gdrive:
 	rclone copy --exclude-from=$COPYIGNORE -v --update ~/GoogleDrive gdrive:
 	rclone copy --exclude-from=$COPYIGNORE -v --update gdrive: ~/GoogleDrive
 fi
