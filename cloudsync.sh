d23920964393f2980708e457a5409974b35a0040
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-01-31 22:39:45 -0600

    use variables

--- cloudsync.sh
+++ cloudsync.sh
@@ -3,25 +3,31 @@
 echo $COPYIGNORE;
 
 if ! [ -f $COPYIGNORE ]; then
-  echo "ERROR"
-  exit 1
+	echo "File missing:" $COPYIGNORE
+	exit 1
+fi
+
+GDRIVE_LOCAL=$HOME/GoogleDrive
+GDRIVE_REMOTE=gdrive:
+
+if ! [ -f $GDRIVE_LOCAL ]; then
+	mkdir -p $GDRIVE_LOCAL
 fi
 
 read -p "override remote files with local files [y/n]?" ans
 if [ $ans = 'y' ]; then
-	rclone sync --exclude-from=$COPYIGNORE -v ~/GoogleDrive gdrive:
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_LOCAL $GDRIVE_REMOTE
 	exit 0
-else
-	read -p "override local files with remote files [y/n]?" ans
-	if [ $ans = 'y' ]; then
-		rclone sync --exclude-from=$COPYIGNORE -v gdrive: ~/GoogleDrive
-		exit 0
-	fi
-
-	echo "running bidirectional copy"
-
-	rclone sync --exclude-from=$COPYIGNORE -v ~/GoogleDrive gdrive:
-	rclone copy --exclude-from=$COPYIGNORE -v --update ~/GoogleDrive gdrive:
-	rclone copy --exclude-from=$COPYIGNORE -v --update gdrive: ~/GoogleDrive
 fi
+
+read -p "override local files with remote files [y/n]?" ans
+if [ $ans = 'y' ]; then
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_REMOTE $GDRIVE_LOCAL
+	exit 0
+fi
+
+echo "running bidirectional copy"
+
+rclone copy --exclude-from=$COPYIGNORE -v --update $GDRIVE_LOCAL $GDRIVE_REMOTE
+rclone copy --exclude-from=$COPYIGNORE -v --update $GDRIVE_REMOTE $GDRIVE_LOCAL
 
