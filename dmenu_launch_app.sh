ea49659ace05e2c871177d35faa24c56f225da5b
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-02-09 23:22:29 -0600

    Synced on 02/09/2024 11:22:29 PM

--- dmenu_launch_app.sh
+++ dmenu_launch_app.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-values=("firefox" "thunderbird" "discord" "chromium")
+values=("firefox" "thunderbird" "discord" "chromium" "libreoffice" "arandr"
+	"font-manager" "virt-manager" "qalculate" "lxappearance" "thunar" "vlc"
+	"pavucontrol" "blueman-manager")
+
 chosen=$(for value in "${values[@]}"; do
     echo $value
-done | dmenu -i -p "Run >")
+done | dmenu -i -p "Run >" -l 20)
+
+if [ -z "$chosen" ]; then
+	exit 0
+fi
 
 if ! which $chosen; then
 	notify-send "aarya" "Application not found: $chosen"
