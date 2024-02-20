d96c3556b8870e15933f9d9c09c129e8572b0dfc
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-02-20 11:17:00 -0600

    Synced on 02/20/2024 11:17:00 AM

--- system/cpu.py
+++ system/cpu.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 import psutil
 import sys
-import numpy as np
 
-average=0
+value=0
 while True:
-    sys.stdout.write(f"CPU: {average}%\n")
+    sys.stdout.write(f"CPU: {value}%\n")
     sys.stdout.flush()
-    values = []
-    for i in range(5):
-        values.append(psutil.cpu_percent(interval=1))
-    average = int(np.mean(values))
+    value = psutil.cpu_percent(interval=5)
