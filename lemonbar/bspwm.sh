3b28a17a70b1ecb7e9da2414fa48648ef8b70ab0
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-03-19 13:19:41 -0500

    Synced on 03/19/2024 01:19:41 PM

--- lemonbar/bspwm.sh
+++ lemonbar/bspwm.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 background="#2D2D2D"
 
-bspc subscribe desktop_focus | while read line; do
-	echo $line
+GetDesktops() {
 	desktops=$(bspc query -D --names | sed "s/Desktop//")
 	focused=$(bspc query -D --names -d focused)
-	# desktops=$(echo $desktops | sed "s/$focused/%{B$background}%{+u}_$focused\_%{-u}%{B-}/")
 	desktops=$(echo $desktops | sed "s/$focused/ [$focused] /")
 	echo $desktops
+}
+
+GetDesktops
+
+bspc subscribe desktop_focus | while read line; do
+	GetDesktops
 done
