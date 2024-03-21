e0b8ed04712e55bb931dffc094cd204e083c9267
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-03-21 16:37:51 -0500

    Synced on 03/21/2024 04:37:51 PM

--- cloudsync.sh
+++ cloudsync.sh
@@ -16,18 +16,30 @@ fi
 
 read -p "override remote files with local files [y/n]?" ans
 if [ $ans = 'y' ]; then
-	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_LOCAL $GDRIVE_REMOTE
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_LOCAL/Desktop $GDRIVE_REMOTE/Desktop
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_LOCAL/Documents $GDRIVE_REMOTE/Documents
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_LOCAL/uiuc $GDRIVE_REMOTE/uiuc
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_LOCAL/Pictures $GDRIVE_REMOTE/Pictures
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_LOCAL/Notes $GDRIVE_REMOTE/Notes
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_LOCAL/Videos $GDRIVE_REMOTE/Videos
 	exit 0
 fi
 
 read -p "override local files with remote files [y/n]?" ans
 if [ $ans = 'y' ]; then
-	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_REMOTE $GDRIVE_LOCAL
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_REMOTE/Desktop $GDRIVE_LOCAL/Desktop
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_REMOTE/Documents $GDRIVE_LOCAL/Documents
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_REMOTE/uiuc $GDRIVE_LOCAL/uiuc
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_REMOTE/Pictures $GDRIVE_LOCAL/Pictures
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_REMOTE/Notes $GDRIVE_LOCAL/Notes
+	rclone sync --exclude-from=$COPYIGNORE -v $GDRIVE_REMOTE/Videos $GDRIVE_LOCAL/Videos
 	exit 0
 fi
 
-echo "running bidirectional copy"
-
-rclone copy --exclude-from=$COPYIGNORE -v --update $GDRIVE_LOCAL $GDRIVE_REMOTE
-rclone copy --exclude-from=$COPYIGNORE -v --update $GDRIVE_REMOTE $GDRIVE_LOCAL
+read -p "bidirectional copy [y/n]?" ans
+if [ $ans = 'y' ]; then
+	rclone copy --exclude-from=$COPYIGNORE -v --update $GDRIVE_LOCAL $GDRIVE_REMOTE
+	rclone copy --exclude-from=$COPYIGNORE -v --update $GDRIVE_REMOTE $GDRIVE_LOCAL
+	exit 0
+fi
 
